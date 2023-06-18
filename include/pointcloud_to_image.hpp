03482023c70c6424d07d67b953c4d78cd7440b83
Basavaraj-PN <bnarasapur@gmail.com>
2023-06-18 12:36:25 +0530

    update

--- include/pointcloud_to_image.hpp
+++ include/pointcloud_to_image.hpp
@@ -4,6 +4,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
+#include <filesystem>
 #include <opencv2/core.hpp>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -15,14 +16,17 @@ typedef pcl::PointCloud<PointT> PointCloudT;
 class PointCloudToImage
 {
 public:
-    PointCloudToImage(const std::string &pointCloudPath, const std::string &imagePath,
-                      const Eigen::Matrix<float, 3, 4> &Tr_, const Eigen::Matrix<float, 3, 4> &P0_);
-    
-    void processPointCloud();
+    PointCloudToImage();
+    void projectPointCloud(const std::string &point_cloud, const std::string &image_path,
+                           const Eigen::Matrix<float, 3, 4> &Tr_, const Eigen::Matrix<float, 3, 4> &P0_,
+                           bool write_image, const std::string output_path);
 
 private:
     std::string point_cloud;
     std::string image_path;
+    cv::Mat image;
+    int imwidth;
+    int imheight;
 
     Eigen::Matrix<float, 3, 4> Tr;
     Eigen::Matrix<float, 3, 4> P0;
@@ -37,6 +41,7 @@ private:
     void filterPointCloud(PointCloudT::Ptr cloud, Axis axis, float threshold);
     Eigen::MatrixXf pointCloudToMatrix(const PointCloudT::Ptr cloud);
     Eigen::MatrixXf filterPointsBehindCamera(const Eigen::MatrixXf &cam_xyz);
+    std::string getImageName(const std::string &imagePath);
 };
 
 #endif // POINTCLOUD_IMAGE_H
